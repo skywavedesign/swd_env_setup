281357b542aebb2439a62c15802a4ed40b67dff8
Jacob Portukalian <jportukalian@gmail.com>
2015-07-23 17:34:55 -0700

    deleted useless files

--- linux/jlink_setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dpkg -i ./jlink_5.0.9_x86_64.deb
-apt-get install -f
--- linux/python32bit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python -c 'import sys; print sys.maxint'
-export VERSIONER_PYTHON_PREFER_32_BIT=yes
-python -c 'import sys; print sys.maxint'
